9cf5e5bfa52e39240d3864d6c2ffc43f4001b9d7
vixhnuchandran <vixhnuchandran@gmail.com>
2024-09-13 15:54:49 +0530

    Hello rust

--- 1.hello_rust/target/release/deps/hello_rust-1888e398c525176c.d
+++ 1.hello_rust/target/release/deps/hello_rust-1888e398c525176c.d
@@ -1,5 +1,5 @@
-/home/user/github.com/vixhnuchandran/rust-beginner-projects/1.hello_rust/target/release/deps/hello_rust-1888e398c525176c: src/main.rs
+/home/user/github.com/vixhnuchandran/rust-projects/1.hello_rust/target/release/deps/hello_rust-1888e398c525176c: src/main.rs
 
-/home/user/github.com/vixhnuchandran/rust-beginner-projects/1.hello_rust/target/release/deps/hello_rust-1888e398c525176c.d: src/main.rs
+/home/user/github.com/vixhnuchandran/rust-projects/1.hello_rust/target/release/deps/hello_rust-1888e398c525176c.d: src/main.rs
 
 src/main.rs:
--- 1.hello_rust/target/release/hello_rust.d
+++ 1.hello_rust/target/release/hello_rust.d
@@ -1,1 +1,1 @@
-/home/user/github.com/vixhnuchandran/rust-beginner-projects/1.hello_rust/target/release/hello_rust: /home/user/github.com/vixhnuchandran/rust-beginner-projects/1.hello_rust/src/main.rs
+/home/user/github.com/vixhnuchandran/rust-projects/1.hello_rust/target/release/hello_rust: /home/user/github.com/vixhnuchandran/rust-projects/1.hello_rust/src/main.rs
